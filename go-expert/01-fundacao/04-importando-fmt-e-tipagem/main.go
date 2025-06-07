4dc659c48725e387087e48aa2bbb3e47b1af9c50
Antonio Henrique Machado <antonio.machado@gupy.com.br>
2025-06-07 02:00:47 -0300

    Importando fmt (ForMaT) e tipagem

---
+++ go-expert/01-fundacao/04-importando-fmt-e-tipagem/main.go
@@ -0,0 +1,12 @@
+package main
+
+// ForMaT -> FMT
+import "fmt"
+
+func main() {
+	var username = "machadoah"
+
+	fmt.Printf("o tipo de username é %T\n", username)  // o tipo de username é string
+	fmt.Printf("o valor de username é %v\n", username) // o valor de username é machadoah
+
+}
